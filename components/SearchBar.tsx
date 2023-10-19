6b936584d512a02c16805ed00381b31b5f0d636c
876-samurai <c.mcintosh876@gmail.com>
2023-10-18 23:52:49 -0400

    Fixed scrolling issues

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -50,7 +50,7 @@ const SearchBar = () => {
       window.location.pathname
     }?${searchParams.toString()}`;
 
-    router.push(newPathname);
+    router.push(newPathname,{scroll:false});
   };
 
   return (
